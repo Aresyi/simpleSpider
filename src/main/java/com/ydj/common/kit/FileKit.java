95606a9f4df71b87f77844630769c3ac2d1ee1e5
易德军 <yidejun@9tongmail.com>
2017-04-28 16:25:29 +0800

    add kit

--- src/main/java/com/ydj/common/kit/FileKit.java
+++ src/main/java/com/ydj/common/kit/FileKit.java
@@ -1,49 +1,65 @@
 package com.ydj.common.kit;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-/**  
+/**
  *
  * @author : Ares.yi
- * @createTime : 2014-11-10 上午11:13:42 
- * @version : 1.0 
- * @description : 
+ * @createTime : 2014-11-10 上午11:13:42
+ * @version : 1.0
+ * @description :
  *
  */
 public class FileKit {
 
-	private FileKit(){}
-	
-	
-	public static void save2File(String saveAsPath,String fileName,String fileContent) throws IOException{
-//		long now = System.currentTimeMillis();
-		
+	private FileKit() {
+	}
+
+	/**
+	 * 写保存文件
+	 * 
+	 * @param saveAsPath
+	 * @param fileName
+	 * @param fileContent
+	 * @param append
+	 * @throws IOException
+	 *
+	 * @author : Ares.yi
+	 * @createTime : 2017年4月28日 下午4:07:08
+	 */
+	public static void save2File(String saveAsPath, String fileName,
+			String fileContent, boolean append) throws IOException {
+		// long now = System.currentTimeMillis();
+
 		FileWriter fw = null;
-		
+
 		File path = new File(saveAsPath);
 		if (!path.exists()) {
 			path.mkdirs();
 		}
-		
-		File file = new File(saveAsPath+fileName);
-		
-//		if(file.exists()){
-//			fileName = now+"_"+fileName;
-//			file = new File(saveAsPath+fileName);
-//		}
-		
-		fw = new FileWriter(file, true);
-		
+
+		File file = new File(saveAsPath + fileName);
+
+		// if(file.exists()){
+		// fileName = now+"_"+fileName;
+		// file = new File(saveAsPath+fileName);
+		// }
+
+		fw = new FileWriter(file, append);
+
 		PrintWriter pw = new PrintWriter(fw);
 		pw.println(fileContent);
 		pw.flush();
-		
+
 		try {
 			fw.flush();
 			pw.close();
@@ -52,22 +68,33 @@ public class FileKit {
 			e.printStackTrace();
 		}
 	}
-	
-	
 
-	public static void save2File(String saveAsPath,String fileName,String fileContent,String charset) {
-		
+	/**
+	 * 写保存文件
+	 * 
+	 * @param saveAsPath
+	 * @param fileName
+	 * @param fileContent
+	 * @param charset
+	 *
+	 * @author : Ares.yi
+	 * @createTime : 2017年4月28日 下午4:07:27
+	 */
+	public static void save2File(String saveAsPath, String fileName,
+			String fileContent, String charset) {
+
 		File path = new File(saveAsPath);
 		if (!path.exists()) {
 			path.mkdirs();
 		}
-		
-		File file = new File(saveAsPath+fileName);
-		
+
+		File file = new File(saveAsPath + fileName);
+
 		BufferedWriter out = null;
 		try {
-			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true),charset));
-			out.write(fileContent+"\r\n");
+			out = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(file, true), charset));
+			out.write(fileContent + "\r\n");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -77,6 +104,44 @@ public class FileKit {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	/**
+	 * 读取文本文件
+	 * 
+	 * @param filePath
+	 * @param encoding
+	 * @return
+	 *
+	 * @author : Ares.yi
+	 * @createTime : 2017年4月28日 下午4:07:40
+	 */
+	public static String readTxtFile(String filePath, String encoding) {
+
+		StringBuffer sbf = new StringBuffer();
+		try {
+			File file = new File(filePath);
+			if (file.isFile() && file.exists()) {
+				InputStreamReader read = new InputStreamReader(
+						new FileInputStream(file), encoding);
+				BufferedReader bufferedReader = new BufferedReader(read);
+				String lineTxt = null;
+				while ((lineTxt = bufferedReader.readLine()) != null) {
+
+					sbf.append(lineTxt);
+
+				}
+				read.close();
+			} else {
+				System.out.println("找不到指定的文件");
+			}
+		} catch (Exception e) {
+			System.out.println("读取文件内容出错");
+			e.printStackTrace();
+		}
+
+		return sbf.toString();
+
 	}
 
 }
